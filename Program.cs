b206b81249fb62c9dcdcf2eb68d8a3822b37c7cf
DiasKapassov <diasgoose@gmail.com>
2023-05-11 22:30:25 +0600

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] arrayStart = new string[14] {"Hello", "world", "!", "My", "name", "is", "Dias", "!", "And", "I", "have", "done", "it", ":)"};
 string[] arrayFinal = new string[arrayStart.Length];
+void ShortArray(string[] arrayStart, string[] arrayFinal)
+
+{
+    int count = 0;
+    for (int i = 0; i < arrayStart.Length; i++)
+    {
+    if(arrayStart[i].Length <= 3)
+        {
+        arrayFinal[count] = arrayStart[i];
+        count++;
+        }
+    }
+}
