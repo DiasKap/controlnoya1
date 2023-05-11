1b8f3e11e4e563778f1b2e7f8bd7cecef4596e29
DiasKapassov <diasgoose@gmail.com>
2023-05-11 22:29:17 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿string[] arrayStart = new string[14] {"Hello", "world", "!", "My", "name", "is", "Dias", "!", "And", "I", "have", "done", "it", ":)"};
+string[] arrayFinal = new string[arrayStart.Length];
